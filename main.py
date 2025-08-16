3fbefae29b4108b35f4388ac5eada95208858939
joaopblume <joaopauloblume@gmail.com>
2025-08-16 11:05:37 -0300

    NEW FEATURE INCOMING

--- main.py
+++ main.py
@@ -30,3 +30,16 @@ def names():
                 id, name = line.strip().split(" - ")
                 names.append({"id": id, "name": name})
             return {"names": names}, 200
+        
+@app.route('/name/<int:name_id>')
+def name(name_id):
+    with open("names.txt", "r") as f:
+        lines = f.readlines()
+        names = []
+        for line in lines:
+            id, name = line.strip().split(" - ")
+            if int(id) == name_id:
+                names.append({"id": id, "name": name})
+                return {"names": names}, 200
+        if not names:
+            return {"error": "ID not found"}, 400
